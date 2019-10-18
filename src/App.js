3da3c606f27eac5f5c6bbcba4de894273fe3232c
Matthew Silva <matthew@enovapoint.com>
2019-10-18 11:13:05 +0300

    updating site url

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 import Routes from './routes';
 import Header from './components/Header';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header/>
       <Routes/>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
