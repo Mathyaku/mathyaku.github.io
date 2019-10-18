1d00ae2a452f751b730116df95d0164978fa3722
Matthew Silva <matthew@enovapoint.com>
2019-10-18 12:02:47 +0300

    Updates

---
+++ precache-manifest.03d7c59f7002ab94eceb9d3b92e89d08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35dfa5a213039e97aa3b1acc550ee462",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22c824753813bb0e8aeb",
+    "url": "/static/css/main.e41984a0.chunk.css"
+  },
+  {
+    "revision": "210fa1209ed679e15c85",
+    "url": "/static/js/2.d4067fbc.chunk.js"
+  },
+  {
+    "revision": "22c824753813bb0e8aeb",
+    "url": "/static/js/main.eddb2c61.chunk.js"
+  },
+  {
+    "revision": "bfd1521e694d93483f19",
+    "url": "/static/js/runtime-main.14a2f347.js"
+  },
+  {
+    "revision": "97dbd0fce19f1dc5bc9ed04d3682b22d",
+    "url": "/static/media/github.97dbd0fc.png"
+  },
+  {
+    "revision": "d9ab0737450b68260e1c4cf35bb392e0",
+    "url": "/static/media/instagram.d9ab0737.png"
+  },
+  {
+    "revision": "d8ea3969fdba20c585c6d0b61bf479c6",
+    "url": "/static/media/linkedin.d8ea3969.png"
+  }
+]);
